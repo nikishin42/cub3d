1cb820f43dc753893f36b5dfe2fa31b2def6b473
Lorent Elle <lelle@student.21-school.ru>
2021-12-04 19:55:36 +0300

    parse after norm

--- sources/parser/error_msg2.c
+++ sources/parser/error_msg2.c
@@ -1,38 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse_check_start_point.c                          :+:      :+:    :+:   */
+/*   error_msg2.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lelle <lelle@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/04 17:05:37 by lelle             #+#    #+#             */
-/*   Updated: 2021/12/04 17:05:39 by lelle            ###   ########.fr       */
+/*   Created: 2021/12/04 17:20:38 by lelle             #+#    #+#             */
+/*   Updated: 2021/12/04 17:20:41 by lelle            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/parse.h"
 
-int	check_start_point(char **map, t_elements *elem)
+int	msg_malloc_fail(void)
 {
-	int	i;
-	int	j;
-
-	i = -1;
-	while (map[++i])
-	{
-		j = -1;
-		while (map[i][++j])
-		{
-			if (ft_strchr("NSEW", map[i][j]))
-			{
-				if (elem->dir != '0')
-					msg_err("More than 1 start point found\n");
-				elem->dir = map[i][j];
-				elem->vec.x = j;
-				elem->vec.y = i;
-			}
-		}		
-	}
-	if (elem->vec.x == -1 && elem->vec.y == -1)
-		msg_err("Start point not found\n");
+	ft_putstr_fd("Error\n", 2);
+	ft_putstr_fd("malloc fail\n", 2);
+	exit(1);
 }
